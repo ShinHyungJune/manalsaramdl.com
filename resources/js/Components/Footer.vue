c6ca01c780046b1a1568b282cd6ac25cff5a6272
ShinHyungJune <ssa4141@naver.com>
2023-01-10 17:31:55 +0900

    fix

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -13,7 +13,7 @@
                 </ul>
             </div>
             <span>대표 : 이태현 &nbsp; 주소 : 서울특별시 강남구 영동대로 602, 6층 &nbsp;<br class="mb" />대표전화 : 1660-1369 &nbsp; 이메일 : cs@insacompany.com &nbsp;</span>
-            <span>통십판매업 신고번호 : 제 2023-서울강남-00152호  &nbsp; 사업자명 : 인사 &nbsp; 사업자등록번호 : 481-17-02165</span>
+            <span>통십판매업 신고번호 : 제 2023-서울강남-00152호  &nbsp; 사업자명 : 인사 &nbsp; <br class="mb" />사업자등록번호 : 481-17-02165</span>
             <span class="copyright">Copyright 인사 All rights reserved.</span>
             <button id="upBtn" class="mb" >
                 <i class="xi-long-arrow-up"></i>
