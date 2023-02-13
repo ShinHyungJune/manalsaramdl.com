ab1f70b81facdfe500c7b339a0c92a439517a340
ShinHyungJune <ssa4141@naver.com>
2023-02-13 12:40:53 +0900

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -10972,7 +10972,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 
-window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
+window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js"); //
+
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
   key: "b37aa6816339a77531ea",
