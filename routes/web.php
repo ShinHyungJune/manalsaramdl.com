4095f891c8a2fc79bed49ad596c20e35c8306dea
ShinHyungJune <ssa4141@naver.com>
2023-03-25 11:24:12 +0900

    테스트생성 추가

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Enums\Sex;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +16,61 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get("/test", function(){
-    dd(explode("#", "#123 #222"));
+    $user = User::factory()->create([
+        "email" => "men2@naver.com",
+        "contact" => "01000000000",
+        "name" => "남자2호",
+        "sex" => Sex::MEN,
+        "birth" => "1995",
+        "job" => "간호사",
+        "school" => "대졸",
+        "city" => "서울시",
+        "area" => "강남구",
+        "need_service" => "소개팅 프로그램",
+        "registration_way" => "인스타그램",
+        "city_company" => "서울시",
+        "area_company" => "강남구",
+
+        "tall" => "174",
+        "weight" => "79",
+        "instagram" => "",
+
+        "ideal" => "이상형",
+        "introduce" => "잘 부탁드려요",
+        "to_manager" => "매칭 잘해주세요",
+        "marriage" => "미혼",
+        "comment_manager" => "이분은 정말 성실한 남성분이세요",
+        "count_dating" => 2,
+        "password" => Hash::make("men2@naver.com")
+    ]);
+
+    $user = User::factory()->create([
+        "email" => "girl2@naver.com",
+        "contact" => "01030217486",
+        "name" => "여자2호",
+        "sex" => Sex::WOMEN,
+        "birth" => "1995",
+        "job" => "간호사",
+        "school" => "대졸",
+        "city" => "서울시",
+        "area" => "강남구",
+        "need_service" => "소개팅 프로그램",
+        "registration_way" => "인스타그램",
+        "city_company" => "서울시",
+        "area_company" => "강남구",
+
+        "tall" => "174",
+        "weight" => "79",
+        "instagram" => "",
+
+        "ideal" => "이상형",
+        "introduce" => "잘 부탁드려요",
+        "to_manager" => "매칭 잘해주세요",
+        "marriage" => "미혼",
+        "comment_manager" => "이분은 정말 성실한 여성분이세요",
+        "count_dating" => 2,
+        "password" => Hash::make("girl2@naver.com")
+    ]);
   //  $product = \App\Models\Product::factory()->create();
 
     // $product->addMedia(storage_path("app\public\main_sec-1_1.jpg"))->preservingOriginal()->toMediaCollection("img", "s3");
